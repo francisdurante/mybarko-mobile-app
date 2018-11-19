dc67517beddf3e631ef9899b7b31d43ee4a35539
Your Name <you@example.com>
2018-11-19 16:18:56 +0800

    activate on backpress

--- app/src/main/java/mybarko/mobile/mybarko/MyBarkoMainActivity.java
+++ app/src/main/java/mybarko/mobile/mybarko/MyBarkoMainActivity.java
@@ -10,13 +10,13 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MyBarkoMainActivity extends AppCompatActivity {
-
+    WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_barko_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("https://mybarko.com/product");
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -29,5 +29,14 @@ public class MyBarkoMainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
